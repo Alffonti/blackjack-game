2749e45406e2c39e7a40b736c53ef009818c53e9
Alf Fonti <alffonti@gmail.com>
2022-06-24 18:17:14 -0300

    update template literals

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // state of the game before starting the game; // declaring variables and assigning its values to 0, false or an empty string. Later a new value will be reassigned depending on the state of the game.
 const player = {
-    name: "Alph",
+    name: 'Alph',
     chips: 145
 }
 let cards = []
 let sum = 0
 let hasBlackJack = false
 let isAlive = false
-let message = ""
+let message = ''
 
-const messageEl = document.getElementById("message-el") // document.querySelector("#message-el")
-const sumEl = document.getElementById("sum-el") 
-const cardsEl = document.getElementById("cards-el")
-const playerEl = document.getElementById("player-el")
+const messageEl = document.getElementById('message-el') // document.querySelector('#message-el')
+const sumEl = document.getElementById('sum-el') 
+const cardsEl = document.getElementById('cards-el')
+const playerEl = document.getElementById('player-el')
 const startGameBtn = document.getElementById('start-game-btn');
 const newCardBtn = document.getElementById('new-card-btn');
 
-playerEl.textContent = player.name + ": $" + player.chips
+playerEl.textContent = `${player.name}: $${player.chips}` 
 
 startGameBtn.addEventListener('click', function startGame() {
     isAlive = true
@@ -28,7 +28,7 @@ startGameBtn.addEventListener('click', function startGame() {
 })
 
 newCardBtn.addEventListener('click', function newCard() {
-    // console.log("Drawing a new card from the deck!")
+    // console.log('Drawing a new card from the deck!')
     // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
     if (isAlive === true && hasBlackJack === false) {
         let card = getRandomCard()
@@ -52,21 +52,21 @@ function getRandomCard() {
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: "
+    cardsEl.textContent = `Cards: `
     // in order to render items on an array on the page I needed to use a 'for' loop (consist of a start condition, finish condition and a step size) // the 'length' property on an array is one number larger than the final index of the array
     for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+        cardsEl.textContent += `${cards[i]} `
     }
     
-    sumEl.textContent = "Sum: " + sum
+    sumEl.textContent = `Sum: ${sum}`
     // The expression inside a conditional is translated into boolean datatype (converting it to true or false value). The condition is executed if it is true.
     if (sum <= 20) {
-        message = "Do you want to draw a new card?"
+        message = `Do you want to draw a new card?`
     } else if (sum === 21) {
-        message = "You've got Blackjack!"
+        message = `You've got Blackjack!`
         hasBlackJack = true
     } else {
-        message = "You're out of the game!"
+        message = `You're out of the game!`
         isAlive = false
     }
     messageEl.textContent = message
